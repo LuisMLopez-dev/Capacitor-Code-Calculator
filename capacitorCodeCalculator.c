472a86fc6d38acd11bd527f06482e6c3c59e0e68
Luis M. Lopez <luismlopez2004@gmail.com>
2025-07-01 12:56:49 -0400

    Update capacitorCodeCalculator.c

--- capacitorCodeCalculator.c
+++ capacitorCodeCalculator.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <math.h>
 
+//Function Prototypes
+void capacitorCodeConverter(int capacitorCode);
+
 int main(void){
-    printf("Hello, world!\n");
+    int userCapacitorCode = 0;
+    
+    do{
+        printf("Enter the code on your capacitor: (Range: 1 to 999) ");
+        scanf("%d", &userCapacitorCode);
+    }while(userCapacitorCode < 1 || userCapacitorCode > 999);
+
+    if(userCapacitorCode < 102){
+        printf("The value of you capacitor is %d \n", userCapacitorCode);
+    }
+    else{
+        capacitorCodeConverter(userCapacitorCode);
+    }
+
+    printf("\nHello, world!\n");
     return 0;
+}
+
+void capacitorCodeConverter(int capacitorCode){
+
+    printf("Function Called\n");
 }
