bfd1e87a3d3f1431ba8477992052c7a825686a7d
Luis M. Lopez <luismlopez2004@gmail.com>
2025-07-01 17:52:40 -0400

    Update capacitorCodeCalculator.c

--- capacitorCodeCalculator.c
+++ capacitorCodeCalculator.c
@@ -1,3 +1,4 @@
+//Header Files
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -9,22 +10,23 @@ int main(void){
     int userCapacitorCode = 0;
     
     do{
-        printf("Enter the code on your capacitor: (Range: 1 to 999) ");
+        printf("Enter the numerical code on your capacitor: (Range: 1 to 999) ");
         scanf("%d", &userCapacitorCode);
-    }while(userCapacitorCode < 1 || userCapacitorCode > 999);
+    }while(userCapacitorCode < 1 || userCapacitorCode > 999); //Checks to see if the user's input is in the valid range of 1 to 999
 
-    if(userCapacitorCode < 102){
-        printf("The value of you capacitor is %d \n", userCapacitorCode);
+    if(userCapacitorCode == 101){ //Checks if the user's capacitor code is specifically 101
+        printf("The value of you capacitor is 100 pF\n");  //If so, print 100 because a capacitor code of 101 is equivalent to 100 pF
+    }
+    else if(userCapacitorCode < 101){ //Else, then check if the user's capacitor code is in the range of 1 to 100
+        printf("The value of your capacitor is %d pF\n", userCapacitorCode); //If so, then the capacitance is equal to the code in pF. For example, code 50 is 50 pF
     }
     else{
-        capacitorCodeConverter(userCapacitorCode);
+        capacitorCodeConverter(userCapacitorCode); //Otherwise, then the code is in the range of 102 to 999
     }
-
-    printf("\nHello, world!\n");
-    return 0;
+    return 0; //End of program
 }
 
-void capacitorCodeConverter(int capacitorCode){
+void capacitorCodeConverter(int capacitorCode){ //Takes the capacitor code of the user and converts it to it's equivalent capacitance
 
     printf("Function Called\n");
 }
